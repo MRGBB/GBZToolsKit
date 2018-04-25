a88886b6e9c67664167d5787761d0462c4d0b377
guobangbang <guobb@pifubao.com>
2018-04-25 09:36:54 +0800

    1

---
+++ Example/Pods/Target Support Files/GBZToolsKit/GBZToolsKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GBZToolsKit : NSObject
+@end
+@implementation PodsDummy_GBZToolsKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-GBZToolsKit_Example/Pods-GBZToolsKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GBZToolsKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GBZToolsKit_Example
+@end
